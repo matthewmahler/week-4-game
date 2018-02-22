c471d32c95ae3e45d749fc7ebf55cd2467c87878
matthewmahler <matthewjamesmahler@gmail.com>
2018-02-22 18:29:38 -0500

    all the things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,15 @@ var $losses = document.getElementById('losses');
 var $target = document.getElementById('target');
 
 //variables
-var target = '';
-var score = '';
+var target = Math.floor((Math.random() * 90) + 10);
+var score = 0;
 var wins = 0;
 var losses = 0;
 var crystal1 = 0;
 var crystal2 = 0;
 var crystal3 = 0;
 var crystal4 = 0;
+
 
 //new game function
 
@@ -24,7 +25,8 @@ function newGame() {
   crystal2 = Math.floor((Math.random() * 10) + 1);
   crystal3 = Math.floor((Math.random() * 10) + 1);
   crystal4 = Math.floor((Math.random() * 10) + 1);
- 
+  
+
   $target.textContent = target;
   document.getElementById("score").textContent = score;
 
@@ -34,48 +36,54 @@ function newGame() {
  console.log(crystal4)
  console.log(target)
  console.log(score)
-
-  // if score = target then win +1
-  //if score > target losses +1
-
+ 
 }
+
+
 
 $("#crystal1").click(function () {
   score = parseInt(score + crystal1);
   document.getElementById("score").textContent = score;
-  check();
+  checkWin();
+  checkLoss();
 });
 
 $("#crystal2").click(function () {
   score = parseInt(score + crystal2);
   document.getElementById("score").textContent = score;
-  check();
+  checkWin();
+  checkLoss();
 });
 
 $("#crystal3").click(function () {
   score = parseInt(score + crystal3);
   document.getElementById("score").textContent = score;
-  check();
+  checkWin();
+  checkLoss();
 });
 
 $("#crystal4").click(function () {
   score = parseInt(score + crystal4);
   document.getElementById("score").textContent = score;
-  check();
+  checkWin();
+  checkLoss();
 });
 
-function check (){
-if (parseInt(score) === target) {
-  var wins = parseInt(wins + 1);
+
+function checkWin (){
+if (score === target) {
+  wins = (wins + 1);
   document.getElementById("wins").textContent = wins;
-  alert("you win!");
+  setTimeout(function() {alert("YOU WIN!"); }, 100);
+ newGame ();
+}}
 
-
-if (parseInt(score) > target) {
-  var losses = parseInt(loses + 1);
+function checkLoss (){
+if (score > target) {
+  losses = (losses + 1);
   document.getElementById("losses").textContent = losses;
-  alert("you lose!");
-}
-}
-}
+  setTimeout(function() {alert("YOU LOSE!"); }, 100);
+  newGame ();
+}}
+
 $newGameButton.addEventListener('click', newGame);
